f53f91b5b99284aedcacb47fce9f3765461cc1a5
fatema-sagar <fatema.sagar@hashedin.com>
2020-02-03 15:38:14 +0530

    Added API to get all products and add a new product

--- src/main/java/com/ecommerce/ecommApp/products/controllers/ProductController.java
+++ src/main/java/com/ecommerce/ecommApp/products/controllers/ProductController.java
@@ -1,7 +1,38 @@
 package com.ecommerce.ecommApp.products.controllers;
 
+import com.ecommerce.ecommApp.commons.pojo.products.Product;
+import com.ecommerce.ecommApp.products.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProductController {
+
+  @Autowired
+  ProductService productService;
+
+  @RequestMapping(value = "/products", method = RequestMethod.GET)
+  private List<Product> getAllProducts() {
+    List<Product> allProducts = new ArrayList<>();
+    allProducts = productService.getProductsList();
+    return allProducts;
+  }
+
+  @RequestMapping(value = "/product/add", method = RequestMethod.POST)
+  private ResponseEntity<Product> addProduct(@RequestBody Product product) {
+    try{
+      return new ResponseEntity<>(productService.createProduct(product), HttpStatus.OK);
+    } catch (Exception e) {
+      e.getMessage();
+    }
+    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+  }
 }
--- src/main/java/com/ecommerce/ecommApp/products/services/ProductService.java
+++ src/main/java/com/ecommerce/ecommApp/products/services/ProductService.java
@@ -1,7 +1,29 @@
 package com.ecommerce.ecommApp.products.services;
 
+import com.ecommerce.ecommApp.commons.pojo.products.Product;
+import com.ecommerce.ecommApp.products.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
+
+  Logger logger = LoggerFactory.getLogger(ProductService.class);
+  @Autowired
+  ProductRepository productRepository;
+
+  public List<Product> getProductsList() {
+    List<Product> productList;
+    productList = productRepository.findAll();
+    return productList;
+  }
+
+  public Product createProduct(Product product) {
+    logger.info("Adding Product {} to the db..", product);
+    return productRepository.save(product);
+  }
 }
