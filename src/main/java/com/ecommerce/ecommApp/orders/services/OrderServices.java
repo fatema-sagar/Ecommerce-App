aa09648c7e4ec65f5bc172703342eb7bc2d7601d
SanchayGupta1197 <sanchay.gupta@hashedin.com>
2020-02-03 17:16:01 +0530

    added basic notification

--- src/main/java/com/ecommerce/ecommApp/orders/services/OrderServices.java
+++ src/main/java/com/ecommerce/ecommApp/orders/services/OrderServices.java
@@ -1,14 +1,27 @@
 package com.ecommerce.ecommApp.orders.services;
 
+import com.ecommerce.ecommApp.EcommAppApplication;
+import com.ecommerce.ecommApp.commons.NotificationProducer;
+import com.ecommerce.ecommApp.commons.Util.CommonsUtil;
+import com.ecommerce.ecommApp.commons.enums.NotificationType;
+import com.ecommerce.ecommApp.commons.pojo.customer.Customer;
+import com.ecommerce.ecommApp.commons.pojo.notification.OrderPlaced;
+import com.ecommerce.ecommApp.commons.pojo.notification.UserRegistered;
+import com.ecommerce.ecommApp.notifications.NotificationUtil;
 import com.ecommerce.ecommApp.orders.Models.Items;
 import com.ecommerce.ecommApp.orders.Models.Orders;
 import com.ecommerce.ecommApp.commons.enums.OrderStatus;
 import com.ecommerce.ecommApp.orders.repository.OrderRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
+import static com.ecommerce.ecommApp.commons.enums.NotificationType.*;
 
 @Service
 public class OrderServices {
@@ -30,6 +43,37 @@ public class OrderServices {
             orders.setCost(item.getCost());
             orders.setStatus(OrderStatus.Placed);
             orderRepository.save(orders);
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            OrderPlaced orderPlaced=new OrderPlaced();
+            //to be replaced by api to get customer
+            Customer customer=new Customer();
+            List<String> modes=new ArrayList<>();
+            modes.add(Text_SMS.toString());
+            modes.add(EMAIL.toString());
+            Long l= 9899889988L;
+            customer.setId(customerID);
+            customer.setName("Sample User");
+            customer.setEmail("sampleemail@mail.com");
+            customer.setNumber(l);
+            customer.setPassword("samplePassword");
+            customer.setGender("Gender");
+            customer.setWhatsapp(l);
+            orderPlaced.setCustomer(customer);
+            orderPlaced.setMode(modes);
+            orderPlaced.setOrderID(orders.getOrderID().toString());
+            //to be replaced by product Api
+            orderPlaced.setProductName("SampleProduct");
+            orderPlaced.setQuandity((int)orders.getQuantity());
+            orderPlaced.setTotalCost((int)orders.getCost());
+
+            NotificationProducer notificationProducer = CommonsUtil.getNotificationProducer();
+            try {
+                notificationProducer.producerNotification(objectMapper.writeValueAsString(orderPlaced),
+                        EcommAppApplication.environment.getRequiredProperty(NotificationUtil.NOTIFICATION_ORDER_PLACED_TOPIC));
+            } catch (IOException ex) {
+
+            }
         }
     }
 
@@ -41,6 +85,7 @@ public class OrderServices {
     public void updateOrderStatus(UUID orderID, OrderStatus status) {
         Orders order= orderRepository.getOne(orderID);
         order.setStatus(status);
+        orderRepository.save(order);
 
     }
 
