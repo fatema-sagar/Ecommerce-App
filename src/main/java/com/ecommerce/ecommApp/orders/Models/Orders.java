ae5fbd4538d6703e334898cbf69ecd1164129107
sagar.bindal <sagar.bindal@hashedin.com>
2020-02-05 12:20:53 +0530

    Order Module code formatted

--- src/main/java/com/ecommerce/ecommApp/orders/Models/Orders.java
+++ src/main/java/com/ecommerce/ecommApp/orders/Models/Orders.java
@@ -1,18 +1,10 @@
 package com.ecommerce.ecommApp.orders.Models;
 
 import com.ecommerce.ecommApp.commons.enums.OrderStatus;
-import lombok.Builder;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 import org.springframework.stereotype.Component;
 
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.UUID;
 @Component
 @Entity
@@ -24,7 +16,7 @@ public class Orders {
 //    @GenericGenerator(
 //            name = "UUID",
 //            strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(updatable = false, nullable = false,unique = true)
+    @Column
     private UUID orderID;
     private Long customerID;
     private Long productID;
@@ -32,16 +24,5 @@ public class Orders {
     private Integer cost;
     @Enumerated
     private OrderStatus status;
-
-    public Orders(Long cId, Long pId, Integer quant, Integer cost, OrderStatus orderStatus)
-    {
-        super();
-        this.orderID=UUID.randomUUID();
-        this.customerID=cId;
-        this.productID=pId;
-        this.quantity=quant;
-        this.cost=cost;
-        this.status=orderStatus;
-    }
 
 }
--- src/main/java/com/ecommerce/ecommApp/orders/services/OrderServices.java
+++ src/main/java/com/ecommerce/ecommApp/orders/services/OrderServices.java
@@ -9,15 +9,12 @@ import com.ecommerce.ecommApp.commons.enums.OrderStatus;
 import com.ecommerce.ecommApp.commons.pojo.customer.CustomerDto;
 import com.ecommerce.ecommApp.commons.pojo.notification.OrderPlaced;
 import com.ecommerce.ecommApp.commons.pojo.orders.ItemsDTO;
-import com.ecommerce.ecommApp.notifications.NotificationUtil;
 import com.ecommerce.ecommApp.orders.Models.Orders;
 import com.ecommerce.ecommApp.orders.repository.OrderRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
@@ -31,13 +28,22 @@ public class OrderServices {
         return orderRepository.getOrdersByCustomerId(customerID);
     }
 
-    public void placeOrder(Long customerID, List<ItemsDTO> productsOrdered) {
-
+    public void placeOrder(Long customerID, List<ItemsDTO> productsOrdered) throws Exception {
         for (ItemsDTO item : productsOrdered) {
-//            Orders orders = new Orders(customerID, item.getProductID(), item.getQuantity(), item.getCost(), OrderStatus.Placed);
-//            orderRepository.save(orders);
-//            notifyUser(Arrays.asList(NotificationType.Text_SMS.toString(), NotificationType.EMAIL.toString()), orders);
+            Orders orders = getOrderInstance(customerID, item);
+            orderRepository.save(orders);
+            notifyUser(Arrays.asList(NotificationType.Text_SMS.toString(), NotificationType.EMAIL.toString()), orders);
         }
+    }
+
+    private Orders getOrderInstance(Long customerId, ItemsDTO item) {
+        Orders orders = new Orders();
+        orders.setCustomerID(customerId);
+        orders.setProductID(item.getProductID());
+        orders.setCost(item.getCost());
+        orders.setQuantity(item.getQuantity());
+        orders.setStatus(OrderStatus.Placed);
+        return orders;
     }
 
 
@@ -56,25 +62,24 @@ public class OrderServices {
         return orderRepository.getOne(orderID);
     }
 
-    private void notifyUser(List<String> modes, Orders order) {
-//        ObjectMapper objectMapper = new ObjectMapper();
-////        notification
-//        OrderPlaced orderPlaced = new OrderPlaced();
-//        String response = Communication.sendGetRequest("localhost:3000/customer/" + order.getCustomerID());
-////        String productName = Communication.sendGetRequest("localhost:3000/prducts/"+order.getProductID());
-//        try {
-//            CustomerDto customer = objectMapper.readValue(response, CustomerDto.class);
-//            orderPlaced.setCustomerDto(customer);
-//            orderPlaced.setMode(modes);
-//            orderPlaced.setOrderID(order.getOrderID().toString());
-//            orderPlaced.setProductName("sample");
-//            orderPlaced.setQuandity(order.getQuantity());
-//            orderPlaced.setTotalCost(order.getCost());
-//            NotificationProducer notificationProducer = CommonsUtil.getNotificationProducer();
-//            notificationProducer.producerNotification(objectMapper.writeValueAsString(orderPlaced),
-//                    EcommAppApplication.environment.getRequiredProperty(NotificationUtil.NOTIFICATION_ORDER_PLACED_TOPIC));
-//        } catch (IOException io) {
-//
-//        }
+    private void notifyUser(List<String> modes, Orders order) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        CustomerDto customer = objectMapper.readValue(Communication.sendGetRequest("https://localhost:/customer/" + order.getCustomerID())
+                , CustomerDto.class);
+        OrderPlaced orderPlaced = createOrderPlacedInstance(modes, order, customer);
+        NotificationProducer notificationProducer = CommonsUtil.getNotificationProducer();
+        notificationProducer.producerNotification(objectMapper.writeValueAsString(orderPlaced),
+                EcommAppApplication.environment.getRequiredProperty("notification.order.placed.topic"));
+    }
+
+    private OrderPlaced createOrderPlacedInstance(List<String> modes, Orders order, CustomerDto customer) {
+        OrderPlaced orderPlaced = new OrderPlaced();
+        orderPlaced.setMode(modes);
+        orderPlaced.setCustomerDto(customer);
+        orderPlaced.setOrderID(order.getOrderID().toString());
+        orderPlaced.setProductName("ABC Product");
+        orderPlaced.setQuandity(orderPlaced.getQuandity());
+        orderPlaced.setTotalCost(orderPlaced.getTotalCost());
+        return orderPlaced;
     }
 }
