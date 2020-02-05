8e7408544ca09d584775e68b466ef1d1ebbfc0ab
krishna <krishna.thakur@hashedin.com>
2020-02-05 17:37:29 +0530

    mapping in the carts pojo done

--- src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Cart.java
+++ src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Cart.java
@@ -1,7 +1,12 @@
 package com.ecommerce.ecommApp.commons.pojo.products;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -10,15 +15,24 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table
+@Table(name = "cart")
+//@Getter
+//@Setter
+//@NoArgsConstructor
 public class Cart {
 
-  @JsonProperty
-  @Id @Getter @NotNull
-  private long customer_id;
+  @Id
+  @Column(name = "cart_id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long cart_id;
 
-  @JsonProperty @NotNull
-  private long product_id;
+
+//  @JsonProperty
+//  @Id @Getter @NotNull
+//  private long customer_id;
+
+//  @JsonProperty @NotNull
+//  private long product_id;
 
   @JsonProperty @NotNull
   @Column(name = "quantity")
@@ -29,12 +43,51 @@ public class Cart {
   Boolean availability;
 
 
-  @ManyToMany(fetch = FetchType.LAZY,
-          cascade = {
-                  CascadeType.PERSIST,
-                  CascadeType.MERGE
-          },
-          mappedBy = "cart")
-  private Set<Inventory> inventories = new HashSet<>();
+@ManyToMany(fetch = FetchType.LAZY,
+        cascade = {
+                CascadeType.PERSIST,
+                CascadeType.MERGE
+        })
+@JoinTable(name = "cart_inventory",
+        joinColumns = { @JoinColumn(name = "cart_id") },
+        inverseJoinColumns = { @JoinColumn(name = "inventoryid") })
+private Set<Inventory> inventory = new HashSet<>();
+
+
+  public Long getCart_id() {
+    return cart_id;
+  }
+
+  public void setCart_id(Long cart_id) {
+    this.cart_id = cart_id;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public Boolean getAvailability() {
+    return availability;
+  }
+
+  public void setAvailability(Boolean availability) {
+    this.availability = availability;
+  }
+
+  public Set<Inventory> getInventory() {
+    return inventory;
+  }
+
+  public void setInventory(Set<Inventory> inventory) {
+    this.inventory = inventory;
+  }
+  public Cart(){
+
+  }
+
 
 }
--- src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Inventory.java
+++ src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Inventory.java
@@ -1,38 +1,89 @@
 package com.ecommerce.ecommApp.commons.pojo.products;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table
+@Table(name = "inventory")
+//@Getter
+//@Setter
+//@NoArgsConstructor
 public class Inventory {
 
   @Id @Column(unique = true)
-  @Getter @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
   long inventoryid;
 
   public enum Size {XS, S, M, L, XL};
-  @Getter @Column
+   @Column(name = "size")
   Size size;
 
-  @Getter @Column
+  @Column(name = "quantity")
   int quantity;
 
-  @Getter @Column
+  @Column(name = "price")
   float price;
 
+
+
+
+  public long getInventoryid() {
+    return inventoryid;
+  }
+
+  public void setInventoryid(long inventoryid) {
+    this.inventoryid = inventoryid;
+  }
+
+  public Size getSize() {
+    return size;
+  }
+
+  public void setSize(Size size) {
+    this.size = size;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public float getPrice() {
+    return price;
+  }
+
+  public void setPrice(float price) {
+    this.price = price;
+  }
 
   @ManyToMany(fetch = FetchType.LAZY,
           cascade = {
                   CascadeType.PERSIST,
                   CascadeType.MERGE
-          })
-  @JoinTable(name = "inventory_cart",
-          joinColumns = { @JoinColumn(name = "inventoryid") },
-          inverseJoinColumns = { @JoinColumn(name = "customer_id") })
-  private Set<Cart> carts = new HashSet<>();
+          },
+          mappedBy = "inventory")
+  private Set<Cart> cart = new HashSet<>();
+
+  public Set<Cart> getCart() {
+    return cart;
+  }
+
+  public void setCart(Set<Cart> cart) {
+    this.cart = cart;
+  }
+
+  public Inventory(){
+
+  }
+
 }
