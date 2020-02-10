1e01cb6b5dc64edace359556f090fdbe772426cb
krishna <krishna.thakur@hashedin.com>
2020-02-10 13:17:13 +0530

    some minor changes

--- src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Cart.java
+++ src/main/java/com/ecommerce/ecommApp/commons/pojo/products/Cart.java
@@ -1,58 +1,44 @@
 package com.ecommerce.ecommApp.commons.pojo.products;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
+import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @Table(name = "cart")
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 public class Cart {
 
-  @Id
-  @Column(name = "cart_id")
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long cart_id;
+    @JsonProperty
+    @NotNull
+    @Column(name = "quantity")
+    int quantity;
+    @JsonProperty
+    @Column(name = "availability")
+    Boolean availability;
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cart_id;
+    @JsonProperty
+    @NotNull
+    @Column(name = "customer_id")
+    private long customerId;
+    @JsonProperty
+    @NotNull
+    private long product_id;
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            },
+            mappedBy = "cart")
 
-
-  @JsonProperty
-  @NotNull
-  private long customer_id;
-
-  @JsonProperty @NotNull
-  private long product_id;
-
-  @JsonProperty @NotNull
-  @Column(name = "quantity")
-  int quantity;
-
-  @JsonProperty
-  @Column(name = "Availability")
-  Boolean availability;
-
-
-@ManyToMany(fetch = FetchType.LAZY,
-        cascade = {
-                CascadeType.PERSIST,
-                CascadeType.MERGE
-        },
-          mappedBy = "cart")
-
-private Set<Product> product = new HashSet<>();
-
-
+    private Set<Product> product = new HashSet<>();
 
 
 }
--- src/main/java/com/ecommerce/ecommApp/products/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/ecommApp/products/repositories/ProductRepository.java
@@ -3,5 +3,10 @@ package com.ecommerce.ecommApp.products.repositories;
 import com.ecommerce.ecommApp.commons.pojo.products.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Override
+    Optional<Product> findById(Integer integer);
 }
