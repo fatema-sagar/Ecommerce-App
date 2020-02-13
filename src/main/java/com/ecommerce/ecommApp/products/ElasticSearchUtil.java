a4bad3b2f0a9df63ec32f105f436cf5c5cd3577f
fatema-sagar <fatema.sagar@hashedin.com>
2020-02-13 13:06:21 +0530

    Added a test case and minor updates

--- src/main/java/com/ecommerce/ecommApp/products/ElasticSearchUtil.java
+++ src/main/java/com/ecommerce/ecommApp/products/ElasticSearchUtil.java
@@ -10,15 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 public final class ElasticSearchUtil {
     private static final String INET_ADDRESS = "localhost:9200";
-    private static final String _INDEX = "products";
+    private static final String _INDEX = "products2";
     private static final String _TYPE = "_doc";
 
     public static boolean insertProduct(Product product) {
         try {
             ObjectMapper objectMapper = CommonsUtil.getObjectMapper();
             String json = objectMapper.writeValueAsString(product);
-            String endpoint = String.format("%s/%s/%s/%s", INET_ADDRESS, _INDEX, _TYPE, product.getProductId());
-            Communication.sendHttpRequest(endpoint, json, RequestMethod.POST);
+            String endpoint = String.format("https://%s/%s/%s/%s", INET_ADDRESS, _INDEX, _TYPE, product.getProductId());
+            System.out.println(endpoint);
+            System.out.println(json);
+            System.out.println(Communication.sendHttpRequest(endpoint, json, RequestMethod.POST));
             return true;
         } catch (Exception ex) {
             return false;
@@ -28,12 +30,27 @@ public final class ElasticSearchUtil {
     private static boolean updateProduct(Product product) {
         try {
             ObjectMapper objectMapper = CommonsUtil.getObjectMapper();
-            String json = objectMapper.writeValueAsString(product);
-            String endpoint = String.format("%s%s%s%s", INET_ADDRESS, _INDEX, _TYPE, product.getProductId());
-            Communication.sendHttpRequest(endpoint, json, RequestMethod.PUT);
+            String productJson = objectMapper.writeValueAsString(product);
+            String jsonBody=String.format("{\"doc\":%s}",productJson);
+            System.out.println(jsonBody);
+            String endpoint = String.format("http://%s%s%s%s", INET_ADDRESS, _INDEX, "_update", product.getProductId());
+            Communication.sendHttpRequest(endpoint, jsonBody, RequestMethod.PUT);
             return true;
         } catch (JsonProcessingException ex) {
             return false;
         }
+    }
+
+    public static void main(String[] args) {
+        Product product = new Product();
+        product.setName("Jeans");
+        product.setProductId(245);
+        product.setCategory("Bottoms");
+        product.setSize("XL");
+        product.setImage("abc");
+        System.out.println(insertProduct(product));
+
+//        product.setName("Ripped Jeans");
+//        updateProduct(product);
     }
 }
