4266d9747dfa20bc487a7f6b800c9771d2d982c4
SanchayGupta1197 <sanchay.gupta@hashedin.com>
2020-02-14 12:26:38 +0530

    added repo in pom

---
+++ src/main/java/com/ecommerce/ecommApp/commons/Util/ElasticSearchQueryBuilder.java
@@ -0,0 +1,9 @@
+package com.ecommerce.ecommApp.commons.Util;
+
+public class ElasticSearchQueryBuilder {
+    public String builtSearchQuery(String jsonInput){
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+
+
+    }
+}
