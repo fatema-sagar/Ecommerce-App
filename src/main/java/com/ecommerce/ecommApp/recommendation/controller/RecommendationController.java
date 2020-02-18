37e377ca057f0b016023b85cab68a696b3213ba7
Amit Verma <amit.verma@hashedin.com>
2020-02-18 11:07:41 +0530

    add controller for recommendation

--- src/main/java/com/ecommerce/ecommApp/recommendation/controller/RecommendationController.java
+++ src/main/java/com/ecommerce/ecommApp/recommendation/controller/RecommendationController.java
@@ -1,17 +1,26 @@
 package com.ecommerce.ecommApp.recommendation.controller;
 
+import com.ecommerce.ecommApp.commons.pojo.products.Product;
+import com.ecommerce.ecommApp.recommendation.service.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/recommendation")
 public class RecommendationController {
 
+    @Autowired
+    private RecommendationService recommendationService;
+
     @GetMapping("/fetch")
     public ResponseEntity fetchRecommendation() {
+//        List<Product> products = recommendationService
         return new ResponseEntity("", HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/ecommApp/view/dto/ViewProductDto.java
+++ src/main/java/com/ecommerce/ecommApp/view/dto/ViewProductDto.java
@@ -1,4 +1,30 @@
 package com.ecommerce.ecommApp.view.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+
+@Data
+@NoArgsConstructor
 public class ViewProductDto {
+
+    @NonNull
+    @JsonProperty("customer_id")
+    private String customerId;
+
+    @NonNull
+    @JsonProperty("product_id")
+    private String productId;
+
+    @JsonProperty("category")
+    private String category;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("brand")
+    private String brand;
+
 }
