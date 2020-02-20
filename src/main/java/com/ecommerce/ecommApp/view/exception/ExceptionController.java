45a68c19440c2745d48fb0a25c8ef2e8268b8933
Amit Verma <amit.verma@hashedin.com>
2020-02-20 12:45:57 +0530

    invoice formate for genrate the invoice

--- src/main/java/com/ecommerce/ecommApp/view/exception/ExceptionController.java
+++ src/main/java/com/ecommerce/ecommApp/view/exception/ExceptionController.java
@@ -1,22 +1,40 @@
 package com.ecommerce.ecommApp.view.exception;
 
 import com.ecommerce.ecommApp.view.dto.response.ApiResponse;
+import com.itextpdf.text.DocumentException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.FileNotFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ApiResponse> handleException(HttpServletRequest req, Exception ex) {
+    public ResponseEntity<ApiResponse> handleRunTimeException(HttpServletRequest req, Exception ex) {
         log.info("Catch Exceptions ----------\n");
         return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
                 .body(new ApiResponse(HttpStatus.NOT_ACCEPTABLE,ex.getMessage(),null));
+    }
+
+    @ExceptionHandler(DocumentException.class)
+    public ResponseEntity<ApiResponse> handleDocumentException(HttpServletRequest request, Exception e) {
+        log.info("Document Exception catch-----------\n");
+        return ResponseEntity
+                .status(HttpStatus.NOT_ACCEPTABLE)
+                .body(new ApiResponse(HttpStatus.NOT_ACCEPTABLE, e.getMessage(),null));
+    }
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<ApiResponse> handleFileNotFountException(HttpServletRequest request, Exception e) {
+        log.info("File not found exception catch---------\n");
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ApiResponse(HttpStatus.NOT_FOUND, e.getMessage(),null));
     }
 
 }
